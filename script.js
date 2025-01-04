863de1707bc5cab6ebaedc8f62b482982dfd3ae5
Екатерина <130153774+cloud00star@users.noreply.github.com>
2025-01-04 15:06:22 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    alert("Добро пожаловать на мой сайт!");
+});
+import { server } from "./script";
+
+// run with `node server.mjs`
+npxhttp - server(http); //localhost:8000)
